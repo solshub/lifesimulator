b6f32e2e10be4e607b3c71c343363353bf1f233a
Sol <53756035+solshub@users.noreply.github.com>
2026-09-22 15:34:57 -0300

    Refactor IndexedDB constants and function calls

--- db.js
+++ db.js
@@ -1,23 +1,23 @@
 import { openDB } from 'idb';
 
 // IndexedDB uses the structure database -> store/table -> entry/row
-const DATABASE_NAME = 'lifesimulator';
-const DATABASE_VERSION = 1;
-const TABLE_NAME = 'authKeys';
+const databaseName = 'lifesimulator';
+const databaseVersion = 1;
+const tableName = 'authKeys';
 
 // set up IndexedDB and upgrade when version changes
 export const openIndexedDB = () => 
-  openDatabase(DATABASE_NAME, DATABASE_VERSION, {
-    upgrade(db) {!db.objectStoreNames.contains(TABLE_NAME) && db.createObjectStore(STORE_NAME)}
+  openDatabase(databaseName, databaseVersion, {
+    upgrade(database) {!database.objectStoreNames.contains(tableName) && database.createObjectStore(tableName)}
   });
 
 // save or load the unique anonymous ID locally using IndexedDB
 export const savePlayerId = async (playerId) => {
-  const db = await openLocalDB();
-  return db.put(TABLE_NAME, playerId, 'current_player_id');
+  const database = await openDatabase();
+  return database.put(tableName, playerId, 'current_player_id');
 };
 
-export const loadPlayerId = async (entry) => {
-  const db = await openLocalDB();
-  return (await db.get(TABLE_NAME,  'current_player_id')) || null;
+export const loadPlayerId = async () => {
+  const database = await openDatabase();
+  return (await database.get(tableName, 'current_player_id')) || null;
 };
