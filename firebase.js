202ae4013d2bc536f85673075f758d7860a89e93
Sol <53756035+solshub@users.noreply.github.com>
2026-09-22 17:10:00 -0300

    Set Firebase configuration with valid credentials
    Updated Firebase configuration with actual credentials.

--- firebase.js
+++ firebase.js
@@ -3,12 +3,12 @@ import { getAuth, signInAnonymously } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyAFD3f-Hb6VPVTmv_vwWuvUg5lX5d0xVPM",
+  authDomain: "lifesimulator-630d6.firebaseapp.com",
+  projectId: "lifesimulator-630d6",
+  storageBucket: "lifesimulator-630d6.firebasestorage.app",
+  messagingSenderId: "128943117741",
+  appId: "1:128943117741:web:a2485350fa3eb72722bd24"
 };
 
 let app, auth, db;
