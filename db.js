5528a8e37e668aa19d638d8288e082976218366a
Sol <matheus.geremias22@gmail.com>
2026-09-22 23:20:40 -0300

    fix(db, cdn): fix IndexedDB upgrade syntax and switch to official CDNs

--- db.js
+++ db.js
@@ -7,17 +7,17 @@ const tableName = 'authKeys';
 
 // set up IndexedDB and upgrade when version changes
 export const openIndexedDB = () => 
-  openDatabase(databaseName, databaseVersion, {
-    upgrade(database) {!database.objectStoreNames.contains(tableName) && database.createObjectStore(tableName)}
+  openDB(databaseName, databaseVersion, {
+    upgrade: (database) => database.objectStoreNames.contains(tableName) ? null : database.createObjectStore(tableName)
   });
-
+ 
 // save or load the unique anonymous ID locally using IndexedDB
 export const savePlayerId = async (playerId) => {
-  const database = await openDatabase();
+  const database = await openIndexedDB();
   return database.put(tableName, playerId, 'current_player_id');
 };
 
 export const loadPlayerId = async () => {
-  const database = await openDatabase();
+  const database = await openIndexedDB();
   return (await database.get(tableName, 'current_player_id')) || null;
 };
