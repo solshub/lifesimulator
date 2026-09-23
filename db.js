059d441dc3c2459c9903326b2016f6fd503b7f1a
Sol <matheus.geremias22@gmail.com>
2026-09-22 23:27:58 -0300

    refactor: replace ternary with logical operator in IndexedDB upgrade handler

--- db.js
+++ db.js
@@ -6,11 +6,14 @@ const databaseVersion = 1;
 const tableName = 'authKeys';
 
 // set up IndexedDB and upgrade when version changes
-export const openIndexedDB = () => 
+export const openIndexedDB = () =>
   openDB(databaseName, databaseVersion, {
-    upgrade: (database) => database.objectStoreNames.contains(tableName) ? null : database.createObjectStore(tableName)
+    upgrade: (database) => {
+      !database.objectStoreNames.contains(tableName) &&
+        database.createObjectStore(tableName);
+    }
   });
- 
+
 // save or load the unique anonymous ID locally using IndexedDB
 export const savePlayerId = async (playerId) => {
   const database = await openIndexedDB();
